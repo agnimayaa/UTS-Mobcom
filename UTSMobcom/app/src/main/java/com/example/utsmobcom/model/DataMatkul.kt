616e41b910546a167480df8a87e363eb5410a300
agnimayaa <agnimaya10@gmail.com>
2023-10-17 14:56:49 +0700

    add

---
+++ UTSMobcom/app/src/main/java/com/example/utsmobcom/model/DataMatkul.kt
@@ -0,0 +1,52 @@
+package com.example.utsmobcom.model
+import com.example.utsmobcom.R
+
+object DataMatkul {
+    val matkul = listOf(
+        Matkul(
+            matkulRes = R.string.matkul_1,
+            sksRes = R.string.sks1,
+            imageRes = R.drawable.unj
+        ),
+        Matkul(
+            matkulRes = R.string.matkul_2,
+            sksRes = R.string.sks2,
+            imageRes = R.drawable.unj
+        ),
+        Matkul(
+            matkulRes = R.string.matkul_3,
+            sksRes = R.string.sks3,
+            imageRes = R.drawable.unj
+        ),
+        Matkul(
+            matkulRes = R.string.matkul_4,
+            sksRes = R.string.sks4,
+            imageRes = R.drawable.unj
+        ),
+        Matkul(
+            matkulRes = R.string.matkul_5,
+            sksRes = R.string.sks5,
+            imageRes = R.drawable.unj
+        ),
+        Matkul(
+            matkulRes = R.string.matkul_6,
+            sksRes = R.string.sks6,
+            imageRes = R.drawable.unj
+        ),
+        Matkul(
+            matkulRes = R.string.matkul_7,
+            sksRes = R.string.sks7,
+            imageRes = R.drawable.unj
+        ),
+        Matkul(
+            matkulRes = R.string.matkul_8,
+            sksRes = R.string.sks8,
+            imageRes = R.drawable.unj
+        ),
+        Matkul(
+            matkulRes = R.string.matkul_9,
+            sksRes = R.string.sks9,
+            imageRes = R.drawable.unj
+        )
+    )
+}
